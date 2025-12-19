ebdb5a852f903133e9a23b110f875f9230cd7596
Martins20321 <jgmsilva11@gmail.com>
2025-12-18 23:04:53 -0300

    Connecting to MongoDB with repository and service

--- src/main/java/com/estudosmartins/workshopmongo/domain/User.java
+++ src/main/java/com/estudosmartins/workshopmongo/domain/User.java
@@ -1,10 +1,15 @@
 package com.estudosmartins.workshopmongo.domain;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@Document(collection = "user") //Sem o collection, o spring entende como o nome da classe, mas em letras minusculas
 public class User implements Serializable {
 
+    @Id
     private String id;
     private String name;
     private String email;
