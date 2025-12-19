4f7e1949010b138645ae46e6dc2e481915ff87c1
Martins20321 <jgmsilva11@gmail.com>
2025-12-19 11:27:00 -0300

    Creating a Post entity with a nested User.

--- src/main/java/com/estudosmartins/workshopmongo/config/Instantiation.java
+++ src/main/java/com/estudosmartins/workshopmongo/config/Instantiation.java
@@ -1,12 +1,17 @@
 package com.estudosmartins.workshopmongo.config;
 
+import com.estudosmartins.workshopmongo.domain.Post;
 import com.estudosmartins.workshopmongo.domain.User;
+import com.estudosmartins.workshopmongo.repository.PostRepository;
 import com.estudosmartins.workshopmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.Date;
+import java.util.TimeZone;
 
 @Configuration
 public class Instantiation implements CommandLineRunner {
@@ -14,15 +19,27 @@ public class Instantiation implements CommandLineRunner {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PostRepository postRepository;
+
     @Override
     public void run(String... args) throws Exception {
 
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+
         userRepository.deleteAll(); //Para deletar oque existia
+        postRepository.deleteAll();
 
         User maria = new User(null, "Maria Brown", "maria@gmail.com");
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
         userRepository.saveAll(Arrays.asList(maria,alex,bob));
+
+        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para São Paulo, abraços!", maria);
+        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje", maria);
+
+        postRepository.saveAll(Arrays.asList(post1,post2));
     }
 }
